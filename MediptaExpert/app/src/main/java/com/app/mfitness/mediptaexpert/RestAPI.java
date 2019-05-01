b5f12df310fcedde8ed38fa6516ace46a0c9b78d
shubhpulate <shubhpulate.1010@gmail.com>
2019-05-01 22:45:31 +0530

    first commit

--- MediptaExpert/app/src/main/java/com/app/mfitness/mediptaexpert/RestAPI.java
+++
@@ -1,9 +0,0 @@
-package com.app.mfitness.mediptaexpert;
-
-public class RestAPI {
-
-    ///public static String dev_api= "http://medipta.org/";
-    public static String dev_api= "http://192.168.100.106/";
-   // public static String dev_api= "http://medipta.in/";
-
-}
